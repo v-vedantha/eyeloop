bd25b2bbf286daccbc98be63a0340074ec61e054
Vedantha Venkatapathy <vedantha@Vedanthas-MacBook-Pro.local>
2022-01-02 11:22:20 -0800

    Ignore unused buffers

--- eyeloop/extractors/custom.py
+++ eyeloop/extractors/custom.py
@@ -15,15 +15,14 @@ from pynput.mouse import Listener, Controller
 
 
 class custom_Extractor:
-    def __init__(self, screen_w=1000, screen_h=1000, camera_w=1000, camera_h=1000, buffer=None):
+    def __init__(self, screen_w=4000, screen_h=3000, camera_w=1000, camera_h=1000, buffer=None):
         self.screen_w = screen_w
         self.screen_h = screen_h
         self.camera_w = camera_w
         self.camera_h = camera_h
         self.world_detector = world_camera.compute_world_cam
         self.predictor = coordinate_mapping.Predictor(screen_w, screen_h, camera_w, camera_h)
-        self.camera = cv2.VideoCapture(0)
-
+        
         Listener(on_click=self.on_click).start()
         #self.kl.start()
 
@@ -52,20 +51,19 @@ class custom_Extractor:
         if input is None:
             return
         output = self.predictor(input)
+        print(output)
         #pyautogui.moveTo(output.x, output.y, duration=1)
         #print(output)
         Controller().position = (output.x, output.y)
     
     def get_input(self):
         if self.buffer is not None:
-            return coordinate_mapping.Input(buffer=self.buffer)
+            current_input = coordinate_mapping.Input(buffer=self.buffer)
+            if current_input.initialized == False:
+                return None
+            return current_input
         if self.most_recent_pupil is None:
             return None
-        frame = self.camera.read()[1]
-        world_points = self.world_detector(frame)
-        if world_points is None:
-            return None
-        return coordinate_mapping.Input(self.most_recent_pupil, world_points[0], world_points[1])
     
     def fetch(self, core):
         #print(core.dataout)
@@ -75,4 +73,4 @@ class custom_Extractor:
         else:
             self.most_recent_pupil = coordinate_mapping.Point(core.dataout["pupil"][0][0], core.dataout["pupil"][0][1])
 
-extractors_add = [custom_Extractor()]
+#extractors_add = [custom_Extractor()]
